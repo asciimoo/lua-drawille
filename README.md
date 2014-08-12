3dee5545408d23a4157c5ea8ca145491959fcdde
Adam Tauber <asciimoo@gmail.com>
2014-08-12 20:33:33 +0200

    [enh] initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+lua-drawille
+============
+
+
+Lua implementation of [drawille](http://github.com/asciimoo/drawille)
+
+
+### Usage
+
+```lua
+Canvas = require "drawille"
+
+local c = Canvas.new()
+
+for i=0,360*4 do
+    if (i % 15) == 0 then
+        c:set(i/15, math.floor(math.sin(i*(math.pi/180))*4))
+    end
+end
+
+c:frame()
+```
+
+
+### Bugs
+
+Bugs or suggestions? Visit the [issue tracker](https://github.com/asciimoo/lua-drawille/issues).
+
+(Tested only with `urxvt` terminal and `fixed` font.)
+
+
+### LICENSE
+
+```
+drawille is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+drawille is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with drawille. If not, see < http://www.gnu.org/licenses/ >.
+
+(C) 2014- by Adam Tauber, <asciimoo@gmail.com>
+```
---
+++ drawille.lua
@@ -0,0 +1,64 @@
+require "bit"
+require "io"
+
+local pixel_map = {{0x01, 0x08},
+                   {0x02, 0x10},
+                   {0x04, 0x20},
+                   {0x40, 0x80}}
+
+-- braille unicode characters starts at 0x2800
+local braille_char_offset = 0x2800
+
+local Canvas = {}
+Canvas.__index = Canvas
+
+function Canvas.new()
+    local self = setmetatable({}, Canvas)
+    self.clear(self)
+    return self
+end
+
+function Canvas.clear(self)
+    self.chars = {}
+end
+
+function Canvas.set(self, x, y)
+    local row = math.floor(y / 4)
+    local col = math.floor(x / 2)
+    if self.chars[row] == nil then
+        self.chars[row] = {}
+    end
+    if self.chars[row][col] == nil then
+        self.chars[row][col] = 0
+    end
+    self.chars[row][col] = bit.bor(self.chars[row][col], pixel_map[(y % 4) + 1][(x % 2) + 1])
+end
+
+function Canvas.frame(self)
+    local mincol, maxcol, minrow, maxrow = 0, 0, 0, 0
+    for rk, rv in pairs(self.chars) do
+        minrow = math.min(rk, minrow)
+        maxrow = math.max(rk, maxrow)
+        for ck, cv in pairs(rv) do
+            mincol = math.min(ck, mincol)
+            maxcol = math.max(ck, maxcol)
+        end
+    end
+    for row=minrow,maxrow do
+        if self.chars[row] then
+            for col=mincol,maxcol do
+                if self.chars[row][col] then
+                    local char = braille_char_offset + self.chars[row][col]
+                    io.stdout:write(string.char(128+64+32+bit.band(15, bit.rshift(char, 12))))
+                    io.stdout:write(string.char(bit.bor(128, bit.band(63, bit.rshift(char, 6)))))
+                    io.stdout:write(string.char(bit.bor(128, bit.band(char, 63))))
+                else
+                    io.stdout:write(" ")
+                end
+            end
+        end
+        io.stdout:write("\n")
+    end
+end
+
+return Canvas
