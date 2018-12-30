5fae47b3eb6f1d7a45dca146d9aff59707a492bb
Hanno Behrens <behrens.hanno@gmail.com>
2018-12-30 19:56:07 +0100

    bug in drawille: removed accidental debug line

--- examples/turtle_mod.lua
+++ examples/turtle_mod.lua
@@ -3,9 +3,10 @@ local Canvas=require"drawille"
 math.randomseed(os.time()^5+os.clock())
 local c = Canvas()
 
-local depth=arg[1] and tonumber(arg[1]) or 3
-local str=arg[2] or "5S90TXAXLBFRAFARFBLXXBXRAFLBFBLFARXAFA"
--- Draw hilbert curve
+local depth=arg[1] and tonumber(arg[1]) or 4
+
+local str=arg[2] or "1S60TXAXFBFRFAFRFBFXXBXFAFLFBFLFAFXA"
+-- Draw Sierpinski arrowhead 
 str=str:lindenmayer(depth)
 print(str)
 c:draw(str)
